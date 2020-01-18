e792cc777764f4f635883600dbaef15dec2079aa
Cameron Abrams <cabrams0@berkeley.edu>
2020-01-17 18:20:55 -0800

    Update clean_data.py

--- house-prices/src/clean_data.py
+++ house-prices/src/clean_data.py
@@ -2,13 +2,49 @@ import pandas as pd
 import numpy as np
 
 
-
-
+# METHODS START ====================================================================
 def categoryCode(dataframe):
     for col_name in dataframe.columns:
         if dataframe[col_name].dtype == 'object':
             dataframe[col_name] = dataframe[col_name].astype('category')
             dataframe[col_name] = dataframe[col_name].cat.codes
+
+def cleanData(data):
+    center(data)
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if np.isnan(data[i,j]) or np.isinf(data[i,j]):
+                data[i, j] = np.nan_to_num(data[i,j]) 
+    scp.normalize(data, norm='l1',axis=1) 
+
+def normalize(data, norm, axis):
+    scp.normalize(data, norm=norm,axis=1) 
+
+def center(data):
+    mean_data_point = get_mean_data_point(data)
+    for index in range(len(data)):
+        data[index] = data[index] - mean_data_point
+
+def get_mean_data_point(data):
+    mean_data_point = data[0]
+    for index in range(1, len(data)):
+        mean_data_point += data[index]
+    mean_data_point = mean_data_point / (len(data))
+    return mean_data_point
+
+def prepData(data, labels):
+    scu.shuffle(data, labels)
+    cleanData(data)
+    return data, labels
+
+def makeContinuous(dataframe, col_name):
+    column = dataframe[col_name].to_numpy()
+    min_val = np.amin(column)
+    max_val = np.amax(column)
+    dataframe[col_name] = dataframe[col_name].apply(lambda x: np.absolute((x-min_val)/(max_val-min_val)))
+    
+
+# METHODS END =========================================================================
 
 train_df = pd.read_csv('train.csv')
 train_df = train_df.drop('SalePrice', axis=1)
@@ -21,17 +57,20 @@ combined = pd.concat([train_df,test_df],axis=0, ignore_index=True)
 print(combined)
 
 categoryCode(combined)
+for col_name in combined.columns:
+    if "year" in col_name.lower():
+        makeContinuous(combined, col_name)
+
 print(combined)
 
 train_df = combined.iloc[:1460,:]
 test_df = combined.iloc[1460:, :]
 
-print(train_df)
-print(test_df)
-
-
 train_df = train_df.drop('Id', axis=1)
 test_df = test_df.drop('Id', axis=1)
+
+print(train_df)
+print(test_df)
 
 train_df.to_csv('data/combined_codefied.csv')
 
